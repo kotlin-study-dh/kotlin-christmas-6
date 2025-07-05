b5d7896ab3c69abe2603d1a61923ee1368d49c95
Kayoung Yoon <dev.kayoung@gmail.com>
2025-07-05 04:09:07 +0200

    feat: added missing policy to the base policy

--- src/main/kotlin/christmas/domain/event/discount/DecemberBasePolicy.kt
+++ src/main/kotlin/christmas/domain/event/discount/DecemberBasePolicy.kt
@@ -1,10 +1,15 @@
 package christmas.domain.event.discount
 
 import christmas.domain.order.Order
+import java.time.LocalDate
 
 
 object DecemberDiscountBasePolicy {
-    private val MIN_PRICE_FOR_EVENT = 10_000
+    private val START_DATE = LocalDate.of(2023, 12, 1)
+    private val END_DATE = LocalDate.of(2023, 12, 31)
+    private const val MIN_PRICE_FOR_EVENT = 10_000
 
-    fun isEligibleForDiscount(order: Order) = order.totalPlacedPrice.value >= MIN_PRICE_FOR_EVENT
+    fun isEligibleForDiscount(order: Order) =
+        order.placedDate in START_DATE..END_DATE
+                && order.totalPlacedPrice.value >= MIN_PRICE_FOR_EVENT
 }
