0242adff860c0470424570e70f1416140306d335
Chungyul <chungyul.lee@deliveryhero.com>
2025-07-11 11:56:18 +0200

    feat: add validations for `Orders`

--- src/main/kotlin/christmas/domain/order/Orders.kt
+++ src/main/kotlin/christmas/domain/order/Orders.kt
@@ -1,17 +1,42 @@
 package christmas.domain.order
 
 import christmas.domain.money.Money
+import christmas.domain.product.ProductType
 
 data class Orders(
     val orders: List<Order>,
 ) {
+    companion object {
+        private const val MIN_ORDER_AMOUNT = 1
+        private const val MAX_ORDER_AMOUNT = 20
+    }
+
     init {
-        require(orders.isNotEmpty()) { "Please order at least one product." }
+        validateOrderNotEmpty()
+        validateProductTypes()
+        validateOrderAmount()
     }
 
     fun totalPrice(): Money {
         val totalAmount = orders.sumOf { it.orderPrice().amount }
         val currency = orders.first().currency()
         return Money(totalAmount, currency)
+    }
+
+    private fun validateOrderNotEmpty() {
+        require(orders.isNotEmpty()) { "Please order at least one product." }
+    }
+
+    private fun validateProductTypes() {
+        if (orders.all { it.product.productType == ProductType.DRINK }) {
+            throw IllegalArgumentException("It is not allowed to order only drinks.")
+        }
+    }
+
+    private fun validateOrderAmount() {
+        val orderAmount = orders.sumOf { it.amount }
+        require(orderAmount in MIN_ORDER_AMOUNT..MAX_ORDER_AMOUNT) {
+            "Order amount should be between $MIN_ORDER_AMOUNT and $MAX_ORDER_AMOUNT."
+        }
     }
 }
--- src/test/kotlin/christmas/domain/order/OrdersTest.kt
+++ src/test/kotlin/christmas/domain/order/OrdersTest.kt
@@ -5,6 +5,7 @@ import christmas.domain.money.Money
 import christmas.domain.product.Product
 import christmas.domain.product.ProductType
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import java.math.BigDecimal
 
@@ -25,5 +26,40 @@ class OrdersTest {
         val orders = Orders(listOf(order1, order2))
 
         assert(orders.totalPrice() == Money(BigDecimal(12.99), Currency.EUR))
+    }
+
+    @Test
+    fun `orders can not contain only DRINKs`() {
+        val drink1 = Product(ProductType.DRINK, "juice", Money(BigDecimal(2.00), Currency.EUR))
+        val drink2 = Product(ProductType.DRINK, "coke", Money(BigDecimal(1.00), Currency.EUR))
+
+        val order1 = Order(drink1, 1)
+        val order2 = Order(drink2, 2)
+
+        assertThrows<IllegalArgumentException> { Orders(listOf(order1, order2)) }
+    }
+
+    @Test
+    fun `success when order amount is 20`() {
+        val product1 = Product(ProductType.MAIN, "steak", Money(BigDecimal(10.99), Currency.EUR))
+        val product2 = Product(ProductType.DRINK, "juice", Money(BigDecimal(2.00), Currency.EUR))
+
+        val order1 = Order(product1, 10)
+        val order2 = Order(product2, 10)
+
+        assertDoesNotThrow { Orders(listOf(order1, order2)) }
+    }
+
+    @Test
+    fun `fail when order amount is 21`() {
+        val product1 = Product(ProductType.MAIN, "steak", Money(BigDecimal(10.99), Currency.EUR))
+        val product2 = Product(ProductType.DRINK, "juice", Money(BigDecimal(2.00), Currency.EUR))
+        val product3 = Product(ProductType.DRINK, "coke", Money(BigDecimal(1.00), Currency.EUR))
+
+        val order1 = Order(product1, 10)
+        val order2 = Order(product2, 10)
+        val order3 = Order(product3, 1)
+
+        assertThrows<IllegalArgumentException> { Orders(listOf(order1, order2, order3)) }
     }
 }
