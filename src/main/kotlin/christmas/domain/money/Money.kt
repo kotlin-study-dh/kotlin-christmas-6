8503e53dcd114e061407ea3f3ac5ac4f3ef82784
Chungyul <chungyul.lee@deliveryhero.com>
2025-07-11 12:13:21 +0200

    refactor: add factory for `Money`

--- src/main/kotlin/christmas/domain/money/Money.kt
+++ src/main/kotlin/christmas/domain/money/Money.kt
@@ -6,6 +6,16 @@ data class Money(
     val amount: BigDecimal,
     val currency: Currency
 ) {
+    companion object {
+        fun intValueOf(amount: Int, currency: Currency): Money {
+            return Money(BigDecimal(amount), currency)
+        }
+
+        fun doubleValueOf(amount: Double, currency: Currency): Money {
+            return Money(BigDecimal(amount), currency)
+        }
+    }
+
     fun multiply(multiplicand: Int): Money {
         return Money(
             amount.multiply(BigDecimal.valueOf(multiplicand.toLong())),
